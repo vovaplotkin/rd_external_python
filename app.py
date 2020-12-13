d71dfae49d6eab48c3e2924584e5f49a9d3d0d98
Volodymyr Plotkin <vovaplotkin95@gmail.com>
2020-12-14 00:20:26 +0200

    fix index form error

--- app.py
+++ app.py
@@ -1,12 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///epam.db'
+db = SQLAlchemy(app)
 
 
-@app.route('/')
+class Departments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    department = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f'<Department {self.id}>'
+
+class Employees(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False)
+    department_id = db.Column(db.Integer, db.ForeignKey('departments.id'), nullable=False)
+    date_of_birth = db.Column(db.Date, nullable=False)
+    salary = db.Column(db.Numeric, nullable=False)
+
+    def __repr__(self):
+        return f'<Employee {self.id}>'
+
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template("index.html")
+    if request.method == 'POST':
+        department = request.form['department']
+        new_dept = Departments(department=department)
+        try:
+            db.session.add(new_dept)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'The error occurs while adding new department'
+    else:
+        departments = Departments.query.all()
+        return render_template("index.html", departments=departments)
 
 
 if __name__ == '__main__':
--- documentation/SRS.md
+++ documentation/SRS.md
@@ -1,3 +1,12 @@
 Application should provide:
-- Storing rental orders, clients and cars in a database;
-- Display list of orders;
+- Storing departments and employees in a database;
+- Display list of departments with the average
+  salary for each;
+- Updating the list of departments 
+  (add, edit, delete);
+- Display list of employees in the departments;
+- Updating the list of employees in the 
+  departments (add, edit, delete);
+- Filtering by date of birth of employees in the
+  departments.
+  
