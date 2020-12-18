3f5e6b4430a561484cfc69f9eefc049002a128c6
Volodymyr Plotkin <vovaplotkin95@gmail.com>
2020-12-18 03:14:17 +0200

    small test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,2 +1,17 @@
 import unittest
+import webapp2
 
+
+class TestApp(unittest.TestCase):
+
+    def setUp(self):
+        self.app = webapp2.app.test_client()
+        self.app.testing = True
+
+    def test_departments(self):
+        home = self.app.get('/')
+        self.assertIn('Departments', str(home.data))
+
+
+if __name__ == "__main__":
+    unittest.main()
